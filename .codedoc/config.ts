c69b5208d7faf6657c3e0078f62ed2c87af966b6
Christian Johnston <blade32232@hotmail.com>
2021-10-29 10:23:21 +1030

    Update config.ts

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -7,7 +7,7 @@ import { theme } from './theme';
 export const config = /*#__PURE__*/configuration({
   theme,                                  // --> add the theme. modify `./theme.ts` for changing the theme.
   dest: {
-    namespace: '/blade32232.github.io'    // --> your github pages namespace. remove if you are using a custom domain.
+    namespace: '/blade32232web'    // --> your github pages namespace. remove if you are using a custom domain.
   },
   page: {
     title: {
@@ -17,7 +17,7 @@ export const config = /*#__PURE__*/configuration({
   misc: {
     github: {
       user: 'blade32232',                 // --> your github username (where your repo is hosted)
-      repo: 'blade32232.github.io',       // --> your github repo name
+      repo: 'blade32232web',       // --> your github repo name
     }
   },
 });
