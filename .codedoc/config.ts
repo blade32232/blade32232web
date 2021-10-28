10e32543477b2eefb1758fd787f83722d1383d27
Christian Johnston <blade32232@hotmail.com>
2021-10-29 09:59:13 +1030

    Codedoc Init

---
+++ .codedoc/config.ts
@@ -0,0 +1,23 @@
+
+import { configuration } from '@codedoc/core';
+
+import { theme } from './theme';
+
+
+export const config = /*#__PURE__*/configuration({
+  theme,                                  // --> add the theme. modify `./theme.ts` for changing the theme.
+  dest: {
+    namespace: '/blade32232.github.io'    // --> your github pages namespace. remove if you are using a custom domain.
+  },
+  page: {
+    title: {
+      base: 'Blade32232.github.io'        // --> the base title of your doc pages
+    }
+  },
+  misc: {
+    github: {
+      user: 'blade32232',                 // --> your github username (where your repo is hosted)
+      repo: 'blade32232.github.io',       // --> your github repo name
+    }
+  },
+});
